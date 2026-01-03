c78455f974e353c2065511f7436dd9023648be59
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-01-03 12:43:35 +0700

    update artifact id

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,16 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import basicSsl from "@vitejs/plugin-basic-ssl"; //comment lại nếu không sài
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // basicSsl(), //comment lại nếu không sài
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      // react: path.resolve(__dirname, "./node_modules/react"),
-      // "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
     },
   },
   server: {
     open: true,
     port: 3000,
-    // host: true, //comment lại nếu không sài
-    // allowedHosts: true,
   },
 });
